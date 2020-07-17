e14226e11cd63ff972cfbfcf9532812e8a3573be
алексей <zheltov86@live.ru>
2020-07-17 17:13:20 +0300

    тест работы модели

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel: ViewModel
     {
+        #region заголовок окна
+        private string _Title = "test";
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+            
+        }
+        #endregion
     }
 }
